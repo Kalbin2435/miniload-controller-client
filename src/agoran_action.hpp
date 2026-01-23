9a431cb7665fb6afef7bf163a0f9135bad4f6502
) <kalbin2435@gmail.com>
2026-01-23 10:00:18 +0200

    yay

--- src/agoran_action.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace main_window {
-    void RenderCustomPopUp();
-    void open_custom_popup();
-    void render_validated_int(const char* text,int &value, bool invalid, int min, int max);
-
-}
